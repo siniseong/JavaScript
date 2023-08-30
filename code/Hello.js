4772a1a2dc29d3faa3499d022195e05976abefff
huise0ng <shinhuiseong07@gmail.com>
2023-08-30 12:06:17 +0900

    ADD: Hello, world!

---
+++ code/Hello.js
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>스크립트 전</p>
+
+  <script>
+    alert( 'Hello, world!' );
+  </script>
+
+  <p>스크립트 후</p>
+
+</body>
+
+</html>
+
+// This code speak Hello, world!
