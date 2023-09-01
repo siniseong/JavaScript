076fed1c1af7f59b1a6ad904b43816d59be8c164
huise0ng <shinhuiseong07@gmail.com>
2023-09-01 09:15:03 +0900

    Formsubmission

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("hi");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+const username = loginInput.value;
+ console.log(username);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
