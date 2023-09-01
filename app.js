23388c248eac1c7fdc80a6dee918141a16e89f63
huise0ng <shinhuiseong07@gmail.com>
2023-09-01 10:11:42 +0900

    Event part 2

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
- event.preventDefault();
- console.log(event);
+  event.preventDefault();
+  console.dir(event);
 }
 
+function handleLinkClick(event) {
+event.preventDefault();
+}
 loginForm.addEventListener("sumbit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+
+handleLinkClick({information about the event that just happend})
 
