bc2b83f7e227806ab3c4337d70904fd7e4769eb8
huise0ng <shinhuiseong07@gmail.com>
2023-09-02 17:02:51 +0900

    ADD: First Drawing

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
 
-const link = document.querySelector("a");
-
-function onLoginSubmit(event) {
-  event.preventDefault();
-  console.dir(event);
-}
-
-function handleLinkClick(event) {
-event.preventDefault();
-}
-loginForm.addEventListener("sumbit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
-
-handleLinkClick({information about the event that just happend})
-
+ctx.strokeRect(50, 50, 100, 200)
